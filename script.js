8d0f894ee3be010664d85648f39492d5d32c2f8b
Matthew <73604168+QWERTYUIOPDADERP@users.noreply.github.com>
2025-04-10 15:48:31 -0700

    worked on flashlight effect

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
-let flashlight = document.getElementById('interactive');
+// let flashlight = document.getElementById('interactive');
+// let mainScrollArrow = document.getElementById('mainScrollArrow');
 
-document.addEventListener('DOMContentLoaded', () => {
-    const interBubble = document.getElementById('interactive');
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
+// let h1 = document.querySelectorAll("h1");
+// let p = document.querySelectorAll("p");
 
-    function move() {
-        curX += (tgX - curX) / 20;
-        curY += (tgY - curY) / 20;
-        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-        requestAnimationFrame(() => {
-            move();
-        });
-    }
+// document.addEventListener('DOMContentLoaded', () => {
+//     const interBubble = document.getElementById('interactive');
+//     let curX = 0;
+//     let curY = 0;
+//     let tgX = 0;
+//     let tgY = 0;
 
-    window.addEventListener('mousemove', (event) => {
-        tgX = event.clientX;
-        tgY = event.clientY;
-    });
+//     function move() {
+//         curX += (tgX - curX) / 20;
+//         curY += (tgY - curY) / 20;
+//         interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+//         requestAnimationFrame(() => {
+//             move();
+//         });
+//     }
 
-    move();
-});
+//     window.addEventListener('mousemove', (event) => {
+//         tgX = event.clientX;
+//         tgY = event.clientY;
+//     });
 
-setTimeout(() => {
-    flashlight.className = 'fadeIn';
-}, 10000);
+//     move();
+// });
 
-setTimeout(() => {
-    flashlight.style.opacity = 0.7;
-}, 14900);
+// setTimeout(() => {
+//     flashlight.className = 'fadeIn';
+// }, 10000);
+
+// setTimeout(() => {
+//     flashlight.style.opacity = 1;
+//     h1.forEach(element => {
+//         element.classList.add('flicker');
+//     });
+//     p.forEach(element => {
+//         element.classList.add('flicker');
+//     });
+//     mainScrollArrow.classList.add('flicker');
+// }, 14900);
+
+$(document).ready(function() {
+    $(this).mousemove(function(e) {
+      $("#light").css({
+        "top": e.pageY - 250,
+        "left": e.pageX - 250
+      })
+    }).mousedown(function(e) {
+      switch (e.which) {
+          case 1:
+              $("#light").toggleClass("light-off");
+              break;
+          case 2:
+              console.log('Middle Mouse button pressed.');
+              break;
+          case 3:
+              console.log('Right Mouse button pressed.');
+              break;
+          default:
+              console.log('You have a strange Mouse!');
+      }
+    })
+  })
