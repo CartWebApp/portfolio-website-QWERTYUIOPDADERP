a7bf33a1c5805527c842809967eba06dbb1ef1b9
Matthew <73604168+QWERTYUIOPDADERP@users.noreply.github.com>
2025-04-11 14:46:10 -0700

    uncommented all the js

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-// let flashlight = document.getElementById('interactive');
-// let mainScrollArrow = document.getElementById('mainScrollArrow');
+let flashlight = document.getElementById('interactive');
+let mainScrollArrow = document.getElementById('mainScrollArrow');
 
-// let h1 = document.querySelectorAll("h1");
-// let p = document.querySelectorAll("p");
+let h1 = document.querySelectorAll("h1");
+let p = document.querySelectorAll("p");
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const interBubble = document.getElementById('interactive');
-//     let curX = 0;
-//     let curY = 0;
-//     let tgX = 0;
-//     let tgY = 0;
+document.addEventListener('DOMContentLoaded', () => {
+    const interBubble = document.getElementById('interactive');
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
 
-//     function move() {
-//         curX += (tgX - curX) / 20;
-//         curY += (tgY - curY) / 20;
-//         interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-//         requestAnimationFrame(() => {
-//             move();
-//         });
-//     }
+    function move() {
+        curX += (tgX - curX) / 20;
+        curY += (tgY - curY) / 20;
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(() => {
+            move();
+        });
+    }
 
-//     window.addEventListener('mousemove', (event) => {
-//         tgX = event.clientX;
-//         tgY = event.clientY;
-//     });
+    window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+    });
 
-//     move();
-// });
+    move();
+});
 
-// setTimeout(() => {
-//     flashlight.className = 'fadeIn';
-// }, 10000);
+setTimeout(() => {
+    flashlight.className = 'fadeIn';
+}, 10000);
 
-// setTimeout(() => {
-//     flashlight.style.opacity = 1;
-//     h1.forEach(element => {
-//         element.classList.add('flicker');
-//     });
-//     p.forEach(element => {
-//         element.classList.add('flicker');
-//     });
-//     mainScrollArrow.classList.add('flicker');
-// }, 14900);
+setTimeout(() => {
+    flashlight.style.opacity = 1;
+    h1.forEach(element => {
+        element.classList.add('flicker');
+    });
+    p.forEach(element => {
+        element.classList.add('flicker');
+    });
+    mainScrollArrow.classList.add('flicker');
+}, 14900);
