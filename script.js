d32069c133988a4e34665b5503d196909e2de731
Matthew <73604168+QWERTYUIOPDADERP@users.noreply.github.com>
2025-05-09 15:41:40 -0700

    chain to toggle flashlight effect

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let flashlight = document.getElementById('interactive');
 let mainScrollArrow = document.getElementById('mainScrollArrow');
+let chain = document.getElementById('pullchain');
 
 let h1 = document.querySelectorAll("h1");
 let p = document.querySelectorAll("p");
+let a = document.querySelectorAll("a");
+let on = false;
+const interBubble = document.getElementById('interactive');
+const cursor = document.getElementById("cursor");
 
 document.addEventListener('DOMContentLoaded', () => {
-    const interBubble = document.getElementById('interactive');
-    const cursor = document.getElementById("cursor");
     let curX = 0;
     let curY = 0;
     let tgX = 0;
@@ -15,9 +18,11 @@ document.addEventListener('DOMContentLoaded', () => {
     function move() {
         curX += (tgX - curX) / 20;
         curY += (tgY - curY) / 20;
-        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-        cursor.style.left = `${Math.round(curX)}px`;
-        cursor.style.top = `${Math.round(curY)}px`;
+        if(!on){
+            interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+            cursor.style.left = `${Math.round(curX)}px`;
+            cursor.style.top = `${Math.round(curY)}px`;
+        }
         requestAnimationFrame(() => {
             move();
         });
@@ -42,8 +47,28 @@ setTimeout(() => {
     h1.forEach(element => {
         element.classList.add('flicker');
     });
+    a.forEach(element => {
+        element.classList.add('flicker');
+    });
     p.forEach(element => {
         element.classList.add('flicker');
     });
     mainScrollArrow.classList.add('flicker');
+    chain.classList.add('letThereBeChain');
 }, 14900);
+
+function toggleLight(){
+    on = !on;
+    interBubble.classList.toggle('on');
+    cursor.classList.toggle('on');
+    h1.forEach(element => {
+        element.classList.toggle('on');
+    });
+    a.forEach(element => {
+        element.classList.toggle('on');
+    });
+    p.forEach(element => {
+        element.classList.toggle('on');
+    });
+    mainScrollArrow.classList.toggle('on');
+}
