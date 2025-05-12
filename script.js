a7a606001b39ced6e8a3cd30f5506996bf05066f
Matthew <73604168+QWERTYUIOPDADERP@users.noreply.github.com>
2025-05-12 15:45:22 -0700

    saves dark/light mode, improvements to visuals

--- script.js
+++ script.js
@@ -3,11 +3,42 @@ let mainScrollArrow = document.getElementById('mainScrollArrow');
 let chain = document.getElementById('pullchain');
 
 let h1 = document.querySelectorAll("h1");
+let h2 = document.querySelectorAll("h2");
 let p = document.querySelectorAll("p");
 let a = document.querySelectorAll("a");
 let on = false;
 const interBubble = document.getElementById('interactive');
 const cursor = document.getElementById("cursor");
+
+let timeoutID = setTimeout(() => {
+    flashlight.className = 'fadeIn';
+    cursor.className = 'overlay fadeIn';
+}, 10000);
+
+let timeoutID2;
+
+function makeThemFlicker(delay){
+    timeoutID2 = setTimeout(() => {
+        flashlight.style.opacity = 1;
+        cursor.style.opacity = 1;
+        h1.forEach(element => {
+            element.classList.add('flicker');
+        });
+        h2.forEach(element => {
+            element.classList.add('flicker');
+        });
+        a.forEach(element => {
+            element.classList.add('flicker');
+        });
+        p.forEach(element => {
+            element.classList.add('flicker');
+        });
+        if(mainScrollArrow){
+            mainScrollArrow.classList.add('flicker');
+        }
+        chain.classList.add('letThereBeChain');
+    }, delay);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     let curX = 0;
@@ -15,9 +46,11 @@ document.addEventListener('DOMContentLoaded', () => {
     let tgX = 0;
     let tgY = 0;
 
+    makeThemFlicker(13000);
+
     function move() {
-        curX += (tgX - curX) / 20;
-        curY += (tgY - curY) / 20;
+        curX += (tgX - curX) / 2;
+        curY += (tgY - curY) / 2;
         if(!on){
             interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
             cursor.style.left = `${Math.round(curX)}px`;
@@ -33,42 +66,54 @@ document.addEventListener('DOMContentLoaded', () => {
         tgY = event.clientY;
     });
 
+    loadLightMode();
+
     move();
 });
 
-setTimeout(() => {
-    flashlight.className = 'fadeIn';
-    cursor.className = 'overlay fadeIn';
-}, 10000);
-
-setTimeout(() => {
-    flashlight.style.opacity = 1;
-    cursor.style.opacity = 1;
-    h1.forEach(element => {
-        element.classList.add('flicker');
-    });
-    a.forEach(element => {
-        element.classList.add('flicker');
-    });
-    p.forEach(element => {
-        element.classList.add('flicker');
-    });
-    mainScrollArrow.classList.add('flicker');
-    chain.classList.add('letThereBeChain');
-}, 14900);
-
-function toggleLight(){
-    on = !on;
+function toggleLight(i=true){
+    if(i) {
+        on = !on;
+    }
     interBubble.classList.toggle('on');
     cursor.classList.toggle('on');
     h1.forEach(element => {
         element.classList.toggle('on');
     });
+    h2.forEach(element => {
+        element.classList.toggle('on');
+    });
     a.forEach(element => {
         element.classList.toggle('on');
     });
     p.forEach(element => {
         element.classList.toggle('on');
     });
-    mainScrollArrow.classList.toggle('on');
+    if(mainScrollArrow){
+        mainScrollArrow.classList.toggle('on');
+    }
+    saveLightMode();
+}
+
+function saveLightMode(){
+    localStorage.setItem(
+        `lightMode`,
+        JSON.stringify((on))
+    );
+}
+
+function loadLightMode(){
+    if(JSON.parse(localStorage.getItem('lightMode'))==null){
+
+    } else {
+        on = JSON.parse(localStorage.getItem('lightMode'))
+        clearTimeout(timeoutID);
+        flashlight.className = 'fadeInQuick';
+        cursor.className = 'overlay fadeInQuick';
+        clearTimeout(timeoutID2);
+        makeThemFlicker(500);
+        if(on){
+            toggleLight(false);
+        }
+    }
 }
